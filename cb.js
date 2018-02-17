c3ab0de31c03afd869f8fa0b4a64d219c50f9847
Mateus Alves <msilva.spa@hotmail.com>
2018-02-17 14:51:55 -0200

    deleting cb.js and adding test modules

--- cb.js
+++
@@ -1,7 +0,0 @@
-module.exports = (callback) => {
-    console.log('funcao cb executada');
-    let n1 = 99;
-    let n2 = 1;
-    n1+=n2;
-    callback('callback executado', 'segundo parametro do cb', n1);
-};
